84338b674a9db5ae215f7dd4e91c28a4349d2db0
Luke In The Jungle <75795165+lukeinthejungle@users.noreply.github.com>
2021-10-11 11:23:53 +0200

    Add files via upload

---
+++ uploaderentities.ps1
@@ -0,0 +1,11 @@
+﻿$fileName='test.txt'
+$fileDestination = 'C:/....'
+$graphqlPath='C:/...'
+$path=$graphqlPath +'/' + $fileName
+$destination=$fileDestination +'/' + $fileName
+$file= Get-Content -Path $path -Raw
+$parsed = AWSModelConverter.exe $path $destination
+
+
+
+aws iotthingsgraph  upload-entity-definitions --document file://$destination
